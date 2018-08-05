a7b66745f8729d7ece29a08c97604ca4e7a575fe
praneet <vasu6657@gmail.com>
2018-08-04 17:33:13 -0700

    Added functionality from WebPoint

--- src/main/java/com/piddamsetty/rpihomecontrol/WebPoint.java
+++ src/main/java/com/piddamsetty/rpihomecontrol/WebPoint.java
@@ -1,5 +1,6 @@
 package com.piddamsetty.rpihomecontrol;
 
+import com.piddamsetty.rpihomecontrol.services.RpiService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebPoint {
 
+    RpiService rpiService = new RpiService();
+
     @RequestMapping("/")
     public String root() {
         System.out.println("New request for home");
         String content = "index.html";
         return content;
+    }
+
+    @RequestMapping("/light1on")
+    public void pin1on() {
+       rpiService.turnOnPin1();
     }
 
 }
